7cdf68069d781e110087df5ce130cd01f879b66e
Mr.April <liuguanqin.g@163.com>
2025-07-29 09:47:54 +0800

    refactor: 系统登录推送未读消息

--- server/app/common/services/system/SysMessageService.php
+++ server/app/common/services/system/SysMessageService.php
@@ -18,7 +18,6 @@ use core\enum\system\MessageStatus;
 use core\notify\enum\PushClientType;
 use core\notify\Notification;
 use madong\helper\Arr;
-use madong\ingenious\libs\utils\ArrayHelper;
 use support\Container;
 
 /**
@@ -44,10 +43,10 @@ class SysMessageService extends BaseService
     {
         try {
             // 获取历史消息记录
-            $models = $this->selectList(['status' => MessageStatus::UNREAD], '*', 0, 0, '', [])->toArray();
+            $models = $this->selectList(['status' => MessageStatus::UNREAD], '*', 0, 0, '', [])->makeVisible('tenant_id')->toArray();
             if (!empty($models)) {
                 foreach ($models as $value) {
-                    Notification::pushOnly(PushClientType::BACKEND, 'admin', '*', $value['receiver_id'], 'message', [], $value, null);
+                    Notification::pushOnly(PushClientType::BACKEND, 'admin', $value['tenant_id'], $value['receiver_id'], 'message', [], $value, null);
                 }
             }
         } catch (\Throwable $e) {
