1a654e4e7599d69f6f78a22e427bbac2222faf0b
Neil <i165@qq.com>
2025-08-08 23:54:17 +0000

    update web/src/api/system/dict/dict.ts. 修复:Dict 类型无法找到，导致TypeScript报错
    Signed-off-by: Neil <i165@qq.com>

--- web/src/api/system/dict/dict.ts
+++ web/src/api/system/dict/dict.ts
@@ -1,23 +1,22 @@
 import BaseApi from '#/api/base-api';
 import { requestClient } from '#/api/request';
-import {DictOptions, Dict} from './dict-model';
-
+import {DictOptions, SystemDictRow} from './dict-model';
 
 
 /**
  * 系统字典 API
  */
-export class SystemDictApi extends BaseApi<Dict> {
+export class SystemDictApi extends BaseApi<SystemDictRow> {
 
   constructor() {
     super('/system/dict');
   }
 
-   async enumDictList(params: any): Promise<Dict[]> {
+   async enumDictList(params: any): Promise<SystemDictRow[]> {
     return requestClient.get(`${this.baseUrl}/enum-dict-list`, {params});
   }
 
-   async customDictList(params: any): Promise<Dict[]> {
+   async customDictList(params: any): Promise<SystemDictRow[]> {
     return requestClient.get(`${this.baseUrl}/custom-dict-list`, {params});
   }
 
